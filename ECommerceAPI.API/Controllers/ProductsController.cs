e5b729bf57193fead3b4da78ebdaa0fa3cd99091
Mustafa Yalçınkaya <myalcinkaya136@gmail.com>
2025-08-11 11:44:32 +0300

    Apply repository design pattern

---
+++ ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductReadRepository _readRepository;
+        private readonly IProductWriteRepository _writeRepository;
+
+        public ProductsController(IProductWriteRepository writeRepository, IProductReadRepository readRepository)
+        {
+            _writeRepository = writeRepository;
+            _readRepository = readRepository;
+        }
+        [HttpGet]
+        public async void Get()
+        {
+            await _writeRepository.AddRangeAsync(new()
+            {
+                new() {Id=Guid.NewGuid(), Name="Product 1", Price = 100, createdDate = DateTime.UtcNow, Stock = 80 },
+                new() {Id=Guid.NewGuid(), Name="Product 2", Price = 200, createdDate = DateTime.UtcNow, Stock = 60 },
+                new() {Id=Guid.NewGuid(), Name="Product 3", Price = 300, createdDate = DateTime.UtcNow, Stock = 20 },
+                new() {Id=Guid.NewGuid(), Name="Product 4", Price = 400, createdDate = DateTime.UtcNow, Stock = 50 },
+                new() {Id=Guid.NewGuid(), Name="Product 5", Price = 500, createdDate = DateTime.UtcNow, Stock = 120 },
+            });
+
+            await _writeRepository.SaveAsync();
+        }
+
+
+    }
+}
